2ee36c17f91a6ad1c25755d126250d14fb068f45
nttrung0301 <trung-nguyenthanh@outlook.com>
2024-08-19 10:17:48 +0700

    Xoa phan microservice

--- microservice/producer/src/modules/shared/base/base.entity.ts
+++
@@ -1,8 +0,0 @@
-import { Prop } from '@nestjs/mongoose';
-
-export class BaseEntity {
-    _id?: string; // Sau này sẽ dùng với class-transformer để serialize dữ liệu response
-    
-    @Prop({ default: null })
-    deleted_at: Date; // Dùng cho soft delete
-}
